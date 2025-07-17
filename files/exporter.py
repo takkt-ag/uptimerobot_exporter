437e0e0bbe0b423a13144ccf78064497923a72b7
Phillip Stockmann <phillip.stockmann@takkt.com>
2025-07-17 13:09:05 +0200

    fix(API): remove account details
    Account details can only be requested with the read-write API key.
    To allow us using the read-only API key for this exporter, we will not get nor render the account information.

--- files/exporter.py
+++ files/exporter.py
@@ -145,17 +145,11 @@ def format_prometheus_psp(data):
 class ReqHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         answer = fetch_data(api_key)
-        accountdetails = fetch_accountdetails(api_key)
         psp = fetch_psp(api_key)
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(format_prometheus(answer.get("monitors")).encode("utf-8"))
-        self.wfile.write(
-            format_prometheus_accountdetails(accountdetails.get("account")).encode(
-                "utf-8"
-            )
-        )
         self.wfile.write(format_prometheus_psp(psp.get("psps")).encode("utf-8"))
 
 
